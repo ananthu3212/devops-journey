5fc97b46e94efb8a4f6e2fc3894480e9d04815a9
Ananthu Chandra Babu <ananthu3212@gmail.com>
2026-04-13 17:04:10 +0200

    Tag 1: Erstes Bash-Script system-info.sh - shebang, echo, command substitution gelernt

---
+++ bash/scripts/system-info.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Gibt den Hostnamen des Servers aus
+echo "Hostname: $(hostname)"
+
+# Gibt den aktuellen Benutzernamen aus
+echo "Current User: $(whoami)"
+
+# Gibt das aktuelle Datum und die aktuelle Uhrzeit aus
+echo "Current Date: $(date)"
